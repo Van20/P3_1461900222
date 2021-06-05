e9b297d5505a21a544c60037549e275860a238e6
unknown <rizvanariyanto@gmail.com>
2021-06-05 21:09:14 +0700

    default

---
+++ app/Http/Controllers/SelectController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Select;
+use App\Models\SelectJenisBuku;
+use App\Models\SelectRakBuku;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SelectController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function addbook()
+    {
+        $addBuku = Select::all();
+
+        return view('add_buku0222', ['addBuku' => $addBuku]);
+    }
+
+    public function viewbook()
+    {
+        $dataBuku = DB::table('rak_buku')
+            ->join('buku', 'id_buku', '=', 'buku.id')
+            ->join('jenis_buku', 'id_jenis_buku', '=', 'jenis_buku.id')
+            ->get();
+
+        return view('index0222', ['dataBuku' => $dataBuku]);
+    }
+
+    public function addjenisbook()
+    {
+        $addJenisBuku = SelectJenisBuku::all();
+
+        return view('add_jenis_buku0222', ['addJenisBuku' => $addJenisBuku]);
+    }
+
+    public function selectLike()
+    {
+        $dataSiswa = DB::table('siswa')
+            ->where('nama', 'like', 'ABI%')
+            ->get();
+
+        $dataSiswa2 = DB::table('siswa')
+            ->where('nama', 'like', 'NOVI%')
+            ->get();
+
+        return view('selectlike0222', ['dataSiswa1' => $dataSiswa, 'dataSiswa2' => $dataSiswa2]);
+    }
+
+    public function selectJoin()
+    {
+        $dtlSiswa = DB::table('siswa')
+            ->join('kelas', 'siswa.id_kelas', '=', 'kelas.id_kelas')
+            ->join('absen', 'siswa.nis', '=', 'absen.nis')
+            ->select('siswa.nis', 'nama', 'kelas', 'tanggal', 'absen', 'id_semester');
+
+        $joindataSiswa = DB::table('semester')
+            ->joinSub($dtlSiswa, 'dtlsiswa', function ($join) {
+                $join->on('semester.id_semester', '=', 'dtlsiswa.id_semester');
+            })->orderBy('nis', 'asc')->get();
+
+        return view('selectjoin0222', ['joindataSiswa' => $joindataSiswa]);
+    }
+
+    public function selectjoinwithWhere()
+    {
+        $dataSiswa = DB::table('siswa')
+            ->leftjoin('kelas', 'siswa.id_kelas', '=', 'kelas.id_kelas')
+            ->where([
+                ['kelas.kelas', '=', 'XI'],
+                ['siswa.nama', 'like', '%A%']
+            ])
+            ->orderBy('nis', 'asc')
+            ->get();
+
+        return view('selectjoinwithwhere0222', ['dataSiswa' => $dataSiswa]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\SelectController;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [SelectController::class, 'viewbook']);
+Route::get('/addBook', [SelectController::class, 'addbook']);
+Route::get('/addJenisBook', [SelectController::class, 'addjenisbook']);
+Route::get('/select', [SelectController::class, 'select']);
+Route::get('/selectlike', [SelectController::class, 'selectlike']);
+Route::get('/selectjoin', [SelectController::class, 'selectjoin']);
+Route::get('/selectjoinwithwhere', [SelectController::class, 'selectjoinwithwhere']);
